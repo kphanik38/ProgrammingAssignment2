a43ab2f9eb265216d61664588e16183ea2f8ff92
kphanik38 <kphanik38@hotmail.com>
2014-06-23 02:05:19 +0530

    Submit the changes with comments incorporated

--- cachematrix.R
+++ cachematrix.R
@@ -9,19 +9,20 @@
 
 makeCacheMatrix <- function(inMatrix = matrix()) {
 
-        outMatrix <- NULL			## set the output matrix to null to clean-up earlier assignments
-		
-		set <- function(y) {		## set the lexical scoping of the variables and cache them
-                inMatrix <<- y
-                outMatrix <<- NULL
-        }
-        
-		get <- function() inMatrix	## function to fetch the input matrix
-        setsolve <- function(solve) outMatrix <<- solve	## use "solve" to inverse and set the matrix in cache
-        getsolve <- function() outMatrix	## fetch the inverse matrix from the cache
-        list(set = set, get = get,	## list all the functions defined with-in the function "makeCacheMatrix"
-             setsolve = setsolve,
-             getsolve = getsolve)
+	outMatrix <- NULL					## set the output matrix to null to clean-up earlier assignments
+
+	set <- function(y) {				## set the lexical scoping of the variables and cache them
+			inMatrix <<- y
+			outMatrix <<- NULL
+	}
+
+	get <- function() inMatrix			## function to fetch the input matrix
+	setsolve <- function(solve) outMatrix <<- solve	## use "solve" to inverse and set the matrix in cache
+	getsolve <- function() outMatrix	## fetch the inverse matrix from the cache
+
+	list(set = set, get = get,			## list all the functions defined with-in the function "makeCacheMatrix"
+			setsolve = setsolve,
+			getsolve = getsolve)
 }
 
 ## "cacheSolve" function computes the inverse of the special "matrix" returned by makeCacheMatrix
@@ -30,17 +31,17 @@ makeCacheMatrix <- function(inMatrix = matrix()) {
 
 cacheSolve <- function(x, ...) {
 
-        outMatrix <- x$getsolve()			## fetch the output (inverse) matrix from cache memory, by "getsolve"
-		
-        if(!is.null(outMatrix)) {			## return the inverse matrix, from cache, if it exists
-                message("Getting/Using the Matrix Inverse from the cache/memory")
-                return(outMatrix)
-        }
+	outMatrix <- x$getsolve()			## fetch the output (inverse) matrix from cache memory, by "getsolve"
 
-        tempMatrix <- x$get()				## get the original matrix from cache 
-		outMatrix <- solve(tempMatrix, ...)	## calculate the inverse of the matrix to ...
-        x$setsolve(outMatrix)				## (a) pin it to cache/memory by calling "setsolve" and
+	if(!is.null(outMatrix)) {			## return the inverse matrix, from cache, if it exists
+		message("Getting/Using the Matrix Inverse from the cache/memory")
+		return(outMatrix)
+	}
 
-        outMatrix							## (b) return it, for the first execution
+	tempMatrix <- x$get()				## get the original matrix from cache 
+	outMatrix <- solve(tempMatrix, ...)	## calculate the inverse of the matrix to ...
+	x$setsolve(outMatrix)				## (a) pin it to cache/memory by calling "setsolve" and
+
+	outMatrix							## (b) return it, for the first execution
 }
 
